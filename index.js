2005b8f0ae1b5df42a750f4abf42401b658895a1
r-k-baral <160538972+r-k-baral@users.noreply.github.com>
2026-05-15 15:37:01 +0530

    added some route

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { ObjectId } from "mongodb";
 const app = express()
 app.use(express.json());
 app.use(cors())
+
 app.post("/add-task",async(req,resq)=>{
     const db = await connection();
      
@@ -46,32 +47,72 @@ app.delete("/delete/:id",async(req,resq)=>{
     
 })
 
-app.put("/task/:id", async (req, resq) => {
-  const { id } = req.params;
+// update 
 
-  try {
-    const db = await connection();
-    const collection = db.collection(collectionname);
-
-    // 🔍 find task
-    const task = await collection.findOne({
-      _id: new ObjectId(id)
-    });
-
-    if (!task) {
-      return resq.send({ success: false, message: "Task not found" });
+app.get("/task/:id", async (req, resq) => {
+    try {
+        const db = await connection();
+        const collection = await db.collection(collectionname)
+        const id = req.params.id
+        const result = await collection.findOne({_id: new ObjectId(id)});
+        
+        if(result){
+            resq.send({massage:'Task fetch', success:true, result})
+        } else {
+            resq.send({massage:'task not fetch', success:false})
+        }
+    } catch (error) {
+        resq.send({ success: false, error });
     }
+})
 
-    const result = await collection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: { completed: !task.completed } }
-    );
+// --- UPDATE WHOLE TASK (Needed for the Save Changes button) ---
+app.put("/tasks/:id", async (req, resq) => {
+    try {
+        const db = await connection();
+        const collection = await db.collection(collectionname);
+        const id = req.params.id;
+        
+        // We remove _id from req.body because MongoDB crashes if you try to update the _id field
+        const { _id, ...updatedData } = req.body; 
 
-    resq.send({ success: true, result });
+        const result = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: updatedData }
+        );
 
-  } catch (error) {
-    resq.send({ success: false, error });
-  }
+        resq.send({ success: true, massage: "Task fully updated", result });
+    } catch (error) {
+        resq.send({ success: false, error });
+    }
 });
+
+// --- TOGGLE CHECKBOX (Needed for the List completion status) ---
+app.put("/task/:id", async (req, resq) => {
+    try {
+        const db = await connection();
+        const collection = await db.collection(collectionname);
+        const id = req.params.id;
+
+        // Find the task first to see if it is currently true or false
+        const task = await collection.findOne({ _id: new ObjectId(id) });
+
+        if (!task) {
+            return resq.send({ success: false, massage: "Task not found" });
+        }
+
+        // Toggle it to the opposite of what it currently is
+        const result = await collection.updateOne(
+            { _id: new ObjectId(id) },
+            { $set: { completed: !task.completed } }
+        );
+
+        resq.send({ success: true, result });
+    } catch (error) {
+        resq.send({ success: false, error });
+    }
+});
+
+
 
 app.listen(3500)
